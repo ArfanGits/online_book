152ea62207ee4236877f6162780bcfe0b6b5f1fb
Arfan A Khan <73901113+ArfanGits@users.noreply.github.com>
2022-01-08 13:22:41 +0600

    php

---
+++ order_confirmation.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Order Information</title>
+  </head>
+  <body>
+  <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span><i class="fas fa-bars"></i></span>
+                </button>
+                <div class="collapse navbar-collapse" id="myNav">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Online Book Store</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="books_catalog.php">Book Catalog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="shopping_cart.php">Shopping Cart</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="order_confirmation.php">Order Confirmation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="payment_by_creditcard.php">Payment</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="signup.php">Registration</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="user_profile.php">User Profile</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+      </nav>
+    </header>
+  
+  
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
